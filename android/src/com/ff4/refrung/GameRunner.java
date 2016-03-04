dad5297f2a4e3ad5c356d9d8a4fe8cc04ef4855f
Martyno <Martyno>
2016-03-04 21:07:45 +0200

    gamerunner

---
+++ android/src/com/ff4/refrung/GameRunner.java
@@ -0,0 +1,19 @@
+package com.ff4.refrung;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
+
+public class GameRunner extends Game{
+	public static AssetManager assets;
+
+	@Override
+	public void create() {
+		
+		assets = new AssetManager();
+		
+	}
+	
+	
+	
+
+}
