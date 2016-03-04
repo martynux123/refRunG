bbf97ad1f8f8e79fb23da94e89e451587588ddcc
Martyno <Martyno>
2016-03-04 20:55:20 +0200

    root

---
+++ android/src/com/ff4/refrung/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ff4.refrung;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ff4.refrung.refrung;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new refrung(), config);
+	}
+}
