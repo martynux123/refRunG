303119757772442ee611ff532872b5ddd736bf28
martynux123 <dargis.martynas@gmail.com>
2016-03-04 23:46:24 +0200

    Font

--- android/src/com/ff4/refrung/GameRunner.java
+++ android/src/com/ff4/refrung/GameRunner.java
@@ -1,11 +1,16 @@
 package com.ff4.refrung;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class GameRunner extends Game{
 	public static AssetManager assets;
+	public static BitmapFont font;
 
 	@Override
 	public void create() {
@@ -22,7 +27,15 @@ public class GameRunner extends Game{
 		assets.load("badlogic.jpg", Texture.class);
 		
 		assets.finishLoading();
+		
+		FreeTypeFontGenerator fontgen = new FreeTypeFontGenerator(Gdx.files.internal("Font.ttf"));
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+		parameter.size = (int) (Gdx.graphics.getHeight()*0.092);
+		font = fontgen.generateFont(parameter);
+		
 		setScreen(new GameMenu(this));
+		
+		
 		
 	}
 	
