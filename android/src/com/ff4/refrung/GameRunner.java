854147359ce04fb86935f1c9a6bdaa97402a0620
martynux123 <dargis.martynas@gmail.com>
2016-03-05 03:42:19 +0200

    Loading screen

--- android/src/com/ff4/refrung/GameRunner.java
+++ android/src/com/ff4/refrung/GameRunner.java
@@ -1,39 +1,48 @@
 package com.ff4.refrung;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+
+import android.os.Handler;
 
 public class GameRunner extends Game{
 	public static AssetManager assets;
 	public static BitmapFont font;
-
+	private SpriteBatch batch;
+	
 	@Override
+	
+	
 	public void create() {
 		
+		GameRunner.assets = new AssetManager();
+		
+		GameRunner.assets.load("Terrorist/Terrorist1.1.png", Texture.class);
+		GameRunner.assets.load("Terrorist/Terrorist1.2.png", Texture.class);
+		GameRunner.assets.load("Terrorist/Terrorist1.3.png", Texture.class);
+		GameRunner.assets.load("Terrorist/Terrorist1.4.png", Texture.class);
+		
+		GameRunner.assets.load("German/German1.1.png", Texture.class);
+		GameRunner.assets.load("German/German1.2.png", Texture.class);
+		GameRunner.assets.load("German/German1.3.png", Texture.class);
+		GameRunner.assets.load("German/German1.4.png", Texture.class);
+		
+		GameRunner.assets.load("Loading.png", Texture.class);
 		
 		
-		assets = new AssetManager();
+		GameRunner.assets.load("badlogic.jpg", Texture.class);
 		
-		assets.load("Terrorist/Terrorist1.1.png", Texture.class);
-		assets.load("Terrorist/Terrorist1.2.png", Texture.class);
-		assets.load("Terrorist/Terrorist1.3.png", Texture.class);
-		assets.load("Terrorist/Terrorist1.4.png", Texture.class);
-		
-		assets.load("German/German1.1.png", Texture.class);
-		assets.load("German/German1.2.png", Texture.class);
-		assets.load("German/German1.3.png", Texture.class);
-		assets.load("German/German1.4.png", Texture.class);
-		
-		
-		
-		assets.load("badlogic.jpg", Texture.class);
-		
-		assets.finishLoading();
+		GameRunner.assets.finishLoading();
+				
 		
 		FreeTypeFontGenerator fontgen = new FreeTypeFontGenerator(Gdx.files.internal("Font.ttf"));
 		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
@@ -45,8 +54,4 @@ public class GameRunner extends Game{
 		
 		
 	}
-	
-	
-	
-
 }
